549c1193e1c6a4233ecec2551c7b1db647fd91b7
Helen Snaith <h.snaith@bodc.ac.uk>
2017-06-27 22:30:01 +0100

    Added OceanSITES ref tables for netCDF generation

---
+++ matlab/netcdf/oceansites_ref_tables.m
@@ -0,0 +1,26 @@
+% oceansites reference tables
+%QC markers
+ref_tab_2={
+'unknown';
+'good_data';
+'probably_good_date';
+'potentially_correctable_bad_data';
+'bad_data';
+'';
+'';
+'nominal_value';
+'interpolated_value';
+'missing_value'};
+%QC procedure level
+ref_tab_3={
+'Raw instrument data';
+'Instrument data that has been converted to geophysical values';
+'Post-recovery calibrations have been applied';
+'Data has been scaled using contextual information';
+'Known bad data has been replaced with null values';
+'Known bad data has been replaced with values based on surrounding data';
+'Ranges applied, bad data flagged ';
+'Data interpolated ';
+'Data manually reviewed ';
+'Data verified against model or other contextual information ';
+'Other QC process applied '};
